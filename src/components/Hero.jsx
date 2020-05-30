544ae80d28f22b7b989f7426dcc51fc0c59bd9ae
Luis LC <luis.lazcanocruz@gmail.com>
2020-05-30 17:02:07 -0500

    Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import profile from '../assets/statics/Profile@2x.png';
+import github from '../assets/statics/Github@2x.png';
+import linkedin from '../assets/statics/Linkdein@2x.png';
+
+const githubLink= 'https://github.com/Luis-Lazcano';
+const linkedinLink= 'https://www.linkedin.com/in/luis-alberto-lazcano-cruz-9a9845148/';
+
+const Hero = () => {
+  return (
+    <div className='Hero'>
+      <h1 className='Hero__text'>Hola soy Luis Alberto,desarrollador de software especilista backend</h1>
+      <div className='Hero__img'>
+        <img className='Hero__img--profile' src={profile} alt='Foto de Perfil' />
+        <a target='_blank' href={githubLink}><img className='Hero__img--github' src={github} alt='Github' /></a>
+        <a target='_blank' href={linkedinLink}><img className='Hero__img--linkedin' src={linkedin} alt='Github' /></a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 import '../assets/styles/App.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Hero />
+      </>
     );
   }
 }
